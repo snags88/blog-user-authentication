40d709e251bf9fd83a4e3ac9b618a88cad2e561f
Seiji Naganuma <Snaganumas@gmail.com>
2015-03-15 23:11:23 -0400

    add all files

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(:email => params[:email])
+    if @user && @user.authenticate(params[:password])
+      login(@user)
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ActiveRecord::Base
+  validates :email, :uniqueness => true, :presence => true
+  has_secure_password
+end
